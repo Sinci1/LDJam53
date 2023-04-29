3bede5147fe3797e086523f74bbf8b088fa95f0d
Sinci1 <sinci1register@gmail.com>
2023-04-29 11:22:25 +0200

    made egg graber

--- Assets/_Project/_Player/Flyer/EggGraber.cs
+++ Assets/_Project/_Player/Flyer/EggGraber.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EggGraber : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    namespace Flyer
     {
-        
-    }
+        public class EggGraber : MonoBehaviour
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            [Header("General")]
+            public bool isSomethingInGrabbingRangeBelow;
+            public bool isEggInGrabbingRangeBelow;
+
+            public bool grabbedEgg;
+            private Egg.BaseScript egg;
+            // Start is called before the first frame update
+            void Start()
+            {
+
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                RaycastHit2D hit = Physics2D.BoxCast(transform.position, (Vector2.one), 0f, Vector2.down, 0.5f, ~LayerMask.GetMask("Player"));
+
+                isSomethingInGrabbingRangeBelow = (hit.collider != null);
+                isEggInGrabbingRangeBelow = (isSomethingInGrabbingRangeBelow && hit.transform.gameObject.GetComponent<Egg.BaseScript>());
+
+                if (Input.GetKeyDown(KeyCode.Space) && isEggInGrabbingRangeBelow){
+                    grabbedEgg = true;
+                    egg = hit.transform.gameObject.GetComponent<Egg.BaseScript>();
+                    egg.GetComponent<Rigidbody2D>().isKinematic = true;
+                    egg.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                }
+
+                if (grabbedEgg) {
+                    egg.transform.position = transform.position + Vector3.down;
+                }
+
+                if (Input.GetKeyUp(KeyCode.Space) && grabbedEgg) {
+                    grabbedEgg = false;
+                    egg.GetComponent<Rigidbody2D>().isKinematic = false;
+                    egg = null;
+                }
+            }
+        }
     }
 }
