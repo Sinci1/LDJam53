a955fb710011206cba5e6321062594033c43e6c9
Sinci1 <sinci1register@gmail.com>
2023-04-30 16:12:41 +0200

    messy code but it does the job

--- Assets/_Project/_Player/Flyer/Winner.cs
+++ Assets/_Project/_Player/Flyer/Winner.cs
@@ -14,17 +14,45 @@ namespace Player
             private EggGraber eggGrab;
             private Transition tr;
 
+            public bool levelBegun;
             public bool beginWinAnimation;
             public bool theEndOfLevel;
             private void Awake()
             {
                 eggGrab = gameObject.GetComponent<EggGraber>();
                 tr = GameObject.FindObjectOfType<Transition>();
+
+                gameObject.GetComponent<Movement>().enabled = false;
+                gameObject.GetComponent<EggGraber>().enabled = false;
+                gameObject.GetComponent<EggSwitcher>().enabled = false;
+                gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
             }
 
             // Update is called once per frame
             void Update()
             {
+                //when level begins
+                if (!levelBegun)
+                {
+                    transform.position += Vector3.right * 5f * Time.deltaTime;
+                    if (transform.position.x > -9.5)
+                    {
+                        gameObject.GetComponent<Movement>().enabled = true;
+                        gameObject.GetComponent<EggGraber>().enabled = true;
+                        gameObject.GetComponent<EggSwitcher>().enabled = true;
+                        gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                        //egg thing
+                        eggGrab.dummyEgg.gameObject.SetActive(false);
+                        eggGrab.trueEgg.gameObject.SetActive(true);
+                        eggGrab.trueEgg.transform.position = eggGrab.dummyEgg.transform.position;
+                        eggGrab.trueEgg.transform.localEulerAngles = Vector3.zero;
+
+                        levelBegun = true;
+                    }
+                }
+                
+
+                //for winning the level
                 if (beginWinAnimation)
                 {
                     transform.position += Vector3.right * 3f * Time.deltaTime;
@@ -34,6 +62,7 @@ namespace Player
                         tr.finishedTransition += ConcludeLevel;
                         theEndOfLevel = true;
                     }
+                    
                 }
             }
             private void OnTriggerEnter2D(Collider2D collision)
