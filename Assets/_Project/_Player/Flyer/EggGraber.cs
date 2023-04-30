e6eba636d0ce5b4b6bfd0b74c87ff6e4677cf165
Sinci1 <sinci1register@gmail.com>
2023-04-30 15:41:42 +0200

    making better egg grab

--- Assets/_Project/_Player/Flyer/EggGraber.cs
+++ Assets/_Project/_Player/Flyer/EggGraber.cs
@@ -8,13 +8,15 @@ namespace Player
     {
         public class EggGraber : MonoBehaviour
         {
+            [Header("To Assign")]
+            public Egg.BaseScript trueEgg;
+            public Transform dummyEgg;
 
             [Header("General")]
             public bool isSomethingInGrabbingRangeBelow;
             public bool isEggInGrabbingRangeBelow;
 
             public bool grabbedEgg;
-            private Egg.BaseScript egg;
             // Start is called before the first frame update
             void Start()
             {
@@ -30,22 +32,26 @@ namespace Player
                 isEggInGrabbingRangeBelow = (isSomethingInGrabbingRangeBelow && hit.transform.gameObject.GetComponent<Egg.BaseScript>());
 
                 if (Input.GetKeyDown(KeyCode.Space) && isEggInGrabbingRangeBelow){
-                    grabbedEgg = true;
+                    /*grabbedEgg = true;
                     egg = hit.transform.gameObject.GetComponent<Egg.BaseScript>();
                     egg.GetComponent<Rigidbody2D>().isKinematic = true;
                     egg.GetComponent<Collider2D>().isTrigger = true;
                     egg.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+                    Debug.Log(hit.distance);*/
+                    trueEgg.gameObject.SetActive(false);
                 }
 
                 if (grabbedEgg) {
-                    egg.transform.position = transform.position + (Vector3.down * 1.3f);
+                    
                 }
 
                 if (Input.GetKeyUp(KeyCode.Space) && grabbedEgg) {
-                    grabbedEgg = false;
+                    trueEgg.gameObject.SetActive(false);
+                    /*grabbedEgg = false;
                     egg.GetComponent<Rigidbody2D>().isKinematic = false;
                     egg.GetComponent<Collider2D>().isTrigger = false;
-                    egg = null;
+                    egg = null;*/
                 }
             }
         }
