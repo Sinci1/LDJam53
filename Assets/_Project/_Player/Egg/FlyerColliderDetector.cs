de0b88c29e8b6aca1751806e3951cd22332f8d9e
Sinci1 <sinci1register@gmail.com>
2023-05-01 22:26:18 +0200

    continuing

--- Assets/_Project/_Player/Egg/FlyerColliderDetector.cs
+++ Assets/_Project/_Player/Egg/FlyerColliderDetector.cs
@@ -8,6 +8,17 @@ namespace Player
     {
         public class FlyerColliderDetector : MonoBehaviour
         {
+            [Header("Player Collider Settings")]
+            public Vector2 size;
+
+            [Header("Checks")]
+            [SerializeField] private bool centerCheck;
+            [SerializeField] private bool upRightCheck;
+            [SerializeField] private bool upLeftCheck;
+            [SerializeField] private bool downRightCheck;
+            [SerializeField] private bool downLeftCheck;
+
+            public bool fullCheck;
             // Start is called before the first frame update
             void Start()
             {
@@ -18,6 +29,35 @@ namespace Player
             void Update()
             {
 
+                Vector2 pos = transform.position;
+                centerCheck = (Physics2D.OverlapAreaAll(pos + (size / 2), pos - (size / 2), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                upRightCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x + size.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                upLeftCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x - size.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                downRightCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x + size.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                downLeftCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x - size.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+
+                fullCheck = centerCheck || upRightCheck || upLeftCheck || downRightCheck || downLeftCheck;
+            }
+
+            bool Check(out Vector3 teleportPosition) {
+                teleportPosition = Vector3.zero;
+                Vector2 pos = transform.position;
+
+                //we preform the checks to see if there are colliders nearby
+                centerCheck = (Physics2D.OverlapAreaAll(pos + (size / 2), pos - (size / 2), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                upRightCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x + size.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                upLeftCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x - size.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                downRightCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x + size.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                downLeftCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x - size.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+
+                
+                if (upRightCheck && upLeftCheck) { teleportPosition = new Vector2(pos.x, pos.y + size.y); }
+                if (downLeftCheck && downRightCheck) { teleportPosition = new Vector2(pos.x, pos.y - size.y); }
+
+                if (centerCheck) { teleportPosition = pos; }
+
+
+                return (centerCheck || upRightCheck || upLeftCheck || downRightCheck || downLeftCheck);
             }
         }
     }
