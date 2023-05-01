b3f7af5dff897e925c70cac5ed1c74143f752296
Sinci1 <sinci1register@gmail.com>
2023-05-01 22:49:04 +0200

    debugging

--- Assets/_Project/_Player/Egg/FlyerColliderDetector.cs
+++ Assets/_Project/_Player/Egg/FlyerColliderDetector.cs
@@ -13,51 +13,70 @@ namespace Player
 
             [Header("Checks")]
             [SerializeField] private bool centerCheck;
+            [SerializeField] private bool upCheck;
+            [SerializeField] private bool downCheck;
+            [SerializeField] private bool rightCheck;
+            [SerializeField] private bool leftCheck;
             [SerializeField] private bool upRightCheck;
             [SerializeField] private bool upLeftCheck;
             [SerializeField] private bool downRightCheck;
             [SerializeField] private bool downLeftCheck;
 
-            public bool fullCheck;
-            // Start is called before the first frame update
-            void Start()
-            {
-
-            }
-
-            // Update is called once per frame
-            void Update()
-            {
-
+            public bool Check(out Vector2 teleportPosition) {
+                teleportPosition = Vector2.zero;
                 Vector2 pos = transform.position;
-                centerCheck = (Physics2D.OverlapAreaAll(pos + (size / 2), pos - (size / 2), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
-                upRightCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x + size.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
-                upLeftCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x - size.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
-                downRightCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x + size.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
-                downLeftCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x - size.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
-
-                fullCheck = centerCheck || upRightCheck || upLeftCheck || downRightCheck || downLeftCheck;
-            }
-
-            bool Check(out Vector3 teleportPosition) {
-                teleportPosition = Vector3.zero;
-                Vector2 pos = transform.position;
+                Vector2 dividedSize = size / 2;
 
                 //we preform the checks to see if there are colliders nearby
-                centerCheck = (Physics2D.OverlapAreaAll(pos + (size / 2), pos - (size / 2), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                centerCheck = (Physics2D.OverlapAreaAll(pos + dividedSize, pos - dividedSize, ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+
+                upCheck = (Physics2D.OverlapAreaAll(new Vector2(pos.x - dividedSize.x, pos.y + size.y), new Vector2(pos.x + dividedSize.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                downCheck = (Physics2D.OverlapAreaAll(new Vector2(pos.x - dividedSize.x, pos.y - size.y), new Vector2(pos.x + dividedSize.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                rightCheck = (Physics2D.OverlapAreaAll(new Vector2(pos.x + size.x, pos.y - dividedSize.y), new Vector2(pos.x + size.x, pos.y + dividedSize.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+                leftCheck = (Physics2D.OverlapAreaAll(new Vector2(pos.x - size.x, pos.y - dividedSize.y), new Vector2(pos.x - size.x, pos.y + dividedSize.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
+
                 upRightCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x + size.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
                 upLeftCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x - size.x, pos.y + size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
                 downRightCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x + size.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
                 downLeftCheck = (Physics2D.OverlapAreaAll(pos, new Vector2(pos.x - size.x, pos.y - size.y), ~LayerMask.GetMask("Egg", "Player")).Length <= 0);
 
-                
-                if (upRightCheck && upLeftCheck) { teleportPosition = new Vector2(pos.x, pos.y + size.y); }
-                if (downLeftCheck && downRightCheck) { teleportPosition = new Vector2(pos.x, pos.y - size.y); }
+                //positions
+                if (upRightCheck) { teleportPosition = new Vector2(pos.x + dividedSize.x, pos.y + dividedSize.y); }
+                if (upLeftCheck) { teleportPosition = new Vector2(pos.x - dividedSize.x, pos.y + dividedSize.y); }
+                if (downRightCheck) { teleportPosition = new Vector2(pos.x + dividedSize.x, pos.y - dividedSize.y); }
+                if (downLeftCheck) { teleportPosition = new Vector2(pos.x - dividedSize.x, pos.y - dividedSize.y); }
+
+                if (upCheck) { teleportPosition = new Vector2(pos.x, pos.y + dividedSize.y); }
+                if (downCheck) { teleportPosition = new Vector2(pos.x, pos.y - dividedSize.y); }
+                if (rightCheck) { teleportPosition = new Vector2(pos.x + dividedSize.x, pos.y); }
+                if (leftCheck) { teleportPosition = new Vector2(pos.x - dividedSize.x, pos.y); }
 
                 if (centerCheck) { teleportPosition = pos; }
 
+                //the end
+                return (centerCheck || upCheck || downCheck || rightCheck || leftCheck || upRightCheck || upLeftCheck || downRightCheck || downLeftCheck);
+            }
+            private void OnDrawGizmos()
+            {
+                if (UnityEditor.EditorApplication.isPlaying)
+                {
+                    Vector3 pos = transform.position;
+                    Vector3 dividedSize = size / 2;
 
-                return (centerCheck || upRightCheck || upLeftCheck || downRightCheck || downLeftCheck);
+                    Gizmos.color = Color.red;
+                    if (upRightCheck) { Gizmos.color = Color.green; } Gizmos.DrawWireCube(new Vector2(pos.x + dividedSize.x, pos.y + dividedSize.y), size); Gizmos.color = Color.red;
+                    if (upLeftCheck) { Gizmos.color = Color.green; } Gizmos.DrawWireCube(new Vector2(pos.x - dividedSize.x, pos.y + dividedSize.y), size); Gizmos.color = Color.red;
+                    if (downRightCheck) { Gizmos.color = Color.green; }  Gizmos.DrawWireCube(new Vector2(pos.x + dividedSize.x, pos.y - dividedSize.y), size); Gizmos.color = Color.red;
+                    if (downLeftCheck) { Gizmos.color = Color.green; } Gizmos.DrawWireCube(new Vector2(pos.x - dividedSize.x, pos.y - dividedSize.y), size); Gizmos.color = Color.red;
+
+                    if (upCheck) { Gizmos.color = Color.green; } Gizmos.DrawWireCube(new Vector2(pos.x, pos.y + dividedSize.y), size); Gizmos.color = Color.red;
+                    if (downCheck) { Gizmos.color = Color.green; } Gizmos.DrawWireCube(new Vector2(pos.x, pos.y - dividedSize.y), size); Gizmos.color = Color.red;
+                    if (rightCheck) { Gizmos.color = Color.green; } Gizmos.DrawWireCube(new Vector2(pos.x + dividedSize.x, pos.y), size); Gizmos.color = Color.red;
+                    if (leftCheck) { Gizmos.color = Color.green; } Gizmos.DrawWireCube(new Vector2(pos.x - dividedSize.x, pos.y), size); Gizmos.color = Color.red;
+
+                    if (centerCheck) { Gizmos.color = Color.green; } Gizmos.DrawWireCube(pos, size); Gizmos.color = Color.red;
+
+                }
             }
         }
     }
--- Assets/_Project/_Player/Flyer/EggSwitcher.cs
+++ Assets/_Project/_Player/Flyer/EggSwitcher.cs
@@ -22,20 +22,24 @@ namespace Player
             void Update()
             {
                 RaycastHit2D hit = Physics2D.BoxCast(transform.position, (Vector2.one), 0f, Vector2.down, 0.5f, ~LayerMask.GetMask("Player", "IgnoreGrab"));
+                Vector2 teleportPos = egg.transform.position;
                 bool isPlatformBelow = (hit.collider != null && hit.transform.GetComponent<PlatformGeneralInfo>());
                 bool isPlatformNestable = (isPlatformBelow && hit.transform.GetComponent<PlatformGeneralInfo>().CanBeNestedOn);
+                bool isEggCheck = egg.GetComponent<Egg.FlyerColliderDetector>().Check(out teleportPos);
 
-                if (Input.GetKeyDown(KeyCode.F) && isPlatformNestable) {
+                if (Input.GetKeyDown(KeyCode.F) && isPlatformNestable && isEggCheck) {
                     Vector3 playerPos = transform.position;
                     Vector3 eggPos = egg.transform.position;
 
                     egg.transform.position = playerPos;
                     egg.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                    transform.position = eggPos;
+                    transform.position = teleportPos;
                 }
 
             }
+            
         }
+
 
     }
 }
